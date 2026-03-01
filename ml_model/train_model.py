e0dd38545076ff73e45e7221690166859999c101
Chukwuemeka Nwoke <chukwuemeka.nwoke@ucdconnect.ie>
2026-03-01 16:10:53 +0000

    Create train_model.py for ML model training
    Upload for EC2 installation 01.03.2026

---
+++ ml_model/train_model.py
@@ -0,0 +1,24 @@
+# train_model.py
+# Run this script ONCE to train and save your ML model
+# Then app.py will load the saved model.pkl for predictions
+
+import pickle
+# import pandas as pd
+# from sklearn.ensemble import RandomForestRegressor
+
+# TODO: Load your historical data
+# df = pd.read_csv('../data/historical.csv')
+
+# TODO: Prepare features (X) and target (y)
+# X = df[['hour', 'day_of_week', 'temperature', 'weather_condition']]
+# y = df['available_bikes']
+
+# TODO: Train model
+# model = RandomForestRegressor()
+# model.fit(X, y)
+
+# TODO: Save model to file
+# with open('model.pkl', 'wb') as f:
+#     pickle.dump(model, f)
+
+print("Model training placeholder - add your ML code here!")
