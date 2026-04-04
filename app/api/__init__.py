16f1cdb6f08c26731bdc067ba164202e0bfbfdaf
Chukwuemeka Nwoke <chukwuemeka.nwoke@ucdconnect.ie>
2026-04-04 23:48:55 +0100

    Update __init__.py

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,5 +1,11 @@
+"""
+app/api/__init__.py - API Blueprint Package Initialization
+Dublin Bikes Web App - COMP30830 Project - Troithean
+"""
+
 from flask import Blueprint
 
 api_bp = Blueprint("api", __name__)
 
+# Import routes to register them with the blueprint
 from app.api import routes
