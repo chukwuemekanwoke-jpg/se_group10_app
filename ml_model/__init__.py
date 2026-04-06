7bbee2fd728f49bd5b3e8bed80dc3197e5179b16
Chukwuemeka Nwoke <chukwuemeka.nwoke@ucdconnect.ie>
2026-04-06 22:09:15 +0100

    Create __init__.py

---
+++ ml_model/__init__.py
@@ -0,0 +1,8 @@
+"""
+Machine Learning model package.
+Handles model training and prediction for bike usage forecasting.
+
+Contents:
+- train_model.py: Model training logic
+- model.pkl: Serialized trained model
+"""
