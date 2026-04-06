596c270633106a63e0a623994dddae5d0b7df1f9
Chukwuemeka Nwoke <chukwuemeka.nwoke@ucdconnect.ie>
2026-04-06 22:09:46 +0100

    Create __init__.py

---
+++ data/__init__.py
@@ -0,0 +1,7 @@
+"""
+Historical and training data directory.
+Contains CSV files and datasets for ML models and analysis.
+
+Contents:
+- historical.csv: Historical bike usage and availability data
+"""
